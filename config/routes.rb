dceec69ccec0ec30659fcb806f0243516664a77b
Diego Stiehl <seccomiro@gmail.com>
2020-03-14 16:04:54 -0300

    Cria CRUD de Doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :doctors
   resources :clinics
   get 'home/index'
   root to: "home#index" 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_185825) do
+ActiveRecord::Schema.define(version: 2020_03_14_190351) do
 
   create_table "clinics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "doctors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
